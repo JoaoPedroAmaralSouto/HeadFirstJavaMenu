873f0ee28e44fa8ba6c21372f5e3e32dad4ea053
JoaoPedroAmaralSouto <jamaralsouto@gmail.com>
2025-12-23 16:56:37 -0300

    Created de board, adding a random numbers of ships

--- Board.java
+++ Board.java
@@ -1,6 +1,41 @@
 package Use_a_cabeca_Java;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class Board {
-    protected ArrayList<String> positionsOfShips;
+    protected ArrayList<Ships> ships = new ArrayList();
+    public void setShips(){
+        Random rand = new Random();
+        int limit = rand.nextInt(4) + 1;
+        while(ships.size() < limit){
+            Ships ship = new Ships();
+            ship.setPosition();
+            if(canPlaceShip(ship))
+                ships.add(ship);
+        }
+    }
 
+    public boolean canPlaceShip(Ship newShip) {
+        for (Ship existing : ships) {
+            if (collides(existing, newShip)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean collides(Ship a, Ship b) {
+        for (String posA : a.getPositions()) {
+            for (String posB : b.getPositions()) {
+                if (posA.equals(posB)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void viewShips(){
+
+    }
 }
--- Ships.java
+++ Ships.java
@@ -4,7 +4,6 @@ import java.util.Random;
 public class Ships{
     protected String[] Position = new String[3];
     protected Random rand = new Random();
-    protected GetIndex getIndex = new GetIndex();
     protected int index = 0;
     protected int indexNext = 0;
     protected int indexNextNumber = 0;
@@ -14,27 +13,26 @@ public class Ships{
         int i = 0;
         int flag = 0;
         int j = 0;
-        ValidateShips validation = new ValidateShips();
         while(i < 3){
             if(i == 0){
                 Position[i] = getFirtPosition();
             }
             else{
-                index = getIndex.index(Position[i - 1], Letters);
-                indexNext = getPositionY();
-                indexNextNumber = getPositionX();
+                index = getIndex(Position[i - 1], Letters);
+                indexNext = setPosition(Letters.length);
+                indexNextNumber = setPosition(Numbers.length);
                 j = 0;
                 while(j < i){
                     flag = 1;
                     if(Letters[indexNext].equals(Position[j].substring(0,1)) && Numbers[indexNextNumber].equals(Position[j].substring(1,2))){
                         while(flag == 1){
-                            indexNextNumber = getPositionX();
+                            indexNextNumber = setPosition(Numbers.length);
                             if(!Numbers[indexNextNumber].equals(Position[j].substring(1,2)))
                                 flag = 0;
                         }
                         else if(!Letters[indexNext].equals(Position[j].substring(0,1)) && !Numbers[indexNextNumber].equals(Position[j].substring(1,2))){
                             while(flag == 1){
-                                indexNextNumber = getPositionX();
+                                indexNextNumber = setPosition(Numbers.length);
                                 if(Numbers[indexNextNumber].equals(Position[j].substring(1,2)))
                                     flag = 0;
                             }
@@ -47,6 +45,10 @@ public class Ships{
             }
             i++;
         }
+    }
+
+    protected String[] getPosition(){
+        return Position;
     }
 
     public String getFirtPosition() {
@@ -77,11 +79,10 @@ public class Ships{
         }
         return aux;
     }
-
-    public int getPositionY() {
+    public int setPosition(int limit){
         int aux = 0;
-        if (index == Letters.length - 1) {
-            aux = rand.nextInt(index + 1) + Letters.length - 2;
+        if (index == limit - 1) {
+            aux = rand.nextInt(index + 1) + limit - 2;
         }
         else if (index == 0) {
             aux = rand.nextInt(index + 2);
@@ -91,18 +92,26 @@ public class Ships{
         }
         return aux;
     }
-
-    public int getPositionX() {
-        int aux = 0;
-        if (indexNext == Numbers.length - 1) {
-            aux = rand.nextInt(index + 1) + Numbers.length - 2;
+    public int getIndex(String position, String [] library){
+        int j = 0;
+        if(library[0].equals("A")){
+            while(j < library.length){
+                if(library[j].equals(position.substring(0,1))){
+                    return j;
+                }
+                j++;
+            }
         }
-        else if (indexNext == 0) {
-            aux = rand.nextInt(index + 2);
+        else{
+            while(j < library.length){
+                if(library[j].equals(position.substring(1,2))){
+                    return j;
+                }
+                j++;
+            }
         }
-        else {
-            aux = rand.nextInt(index + 2) + index - 1;
-        }
-        return aux;
+        return -1;
     }
 }
+
+
